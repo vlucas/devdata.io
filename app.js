f89996a07d5be6895c695e432d7af92b40a519f5
Vance Lucas <vance@vancelucas.com>
2015-07-14 12:26:11 -0500

    Add Canadian Provinces and use datasets hash

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ var router = express.Router();
  * Config
  */
 var supportedFormats = ['json', 'php', 'python', 'ruby'];
-var datasets = ['states', 'countries', 'currencies'];
+var datasets = {
+  'states': 'U.S. States',
+  'canadian-provinces': 'Canadian Provinces',
+  'countries': 'World Countries',
+  'currencies': 'World Currencies'
+};
 
 /**
  * Express
@@ -25,55 +30,55 @@ app.engine('jade', require('jade').__express);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.render('index', { languages: supportedFormats });
-});
-
-app.get('/datasets/new', function(req, res) {
-  res.render('datasets/new');
+  res.render('index', { languages: supportedFormats, datasets: datasets });
 });
 
 
 // Datasets
-datasets.forEach(function(dataset) {
-  app.get('/datasets/' + dataset, function(req, res) {
-    var errMsg;
+app.get('/datasets/:dataset', function(req, res) {
+  var errMsg;
+  var dataset = req.params.dataset;
 
-    // Ensure we have a format
-    if (!req.query.lang) {
-      errMsg = "Code language not specified. Please choose a code language with the 'lang' parameter.";
+  // Ensure we have a valid dataset
+  if (datasets[dataset] == "undefined") {
+    errMsg = "Invalid dataset";
+  }
 
-    // Ensure format is one we support
-    } else if (supportedFormats.indexOf(req.query.lang) === -1) {
-      errMsg = "Code language '" + req.query.lang + "' not supported. Supported languages are: ['" + supportedFormats.join("', '") + "'].";
+  // Ensure we have a format
+  if (!req.query.lang) {
+    errMsg = "Code language not specified. Please choose a code language with the 'lang' parameter.";
+
+  // Ensure format is one we support
+  } else if (supportedFormats.indexOf(req.query.lang) === -1) {
+    errMsg = "Code language '" + req.query.lang + "' not supported. Supported languages are: ['" + supportedFormats.join("', '") + "'].";
+  }
+
+  // Show error if set and break
+  if (errMsg) {
+    return res.status(400).json({ status: 'error', message: errMsg });
+  }
+
+  // Read data from JSON file
+  fs.readFile(__dirname + '/datasets/' + dataset + '.json', function readFileDataClosure (err, data) {
+    if (err) {
+      return res.status(500).json({ status: 'error', message: 'Server error: Unable to read source data file' });
     }
 
-    // Show error if set and break
-    if (errMsg) {
-      return res.status(400).json({ status: 'error', message: errMsg });
+    // Format data for specified programming language
+    // ... ?
+    var dataLang;
+    var jsonData = JSON.parse(data);
+
+    dataLang = converter.jsonToLang(jsonData, req.query.lang);
+    if (!dataLang) {
+      return res.status(400).json({ status: 'error', message: 'Unsupported lang' });
     }
 
-    // Read data from JSON file
-    fs.readFile(__dirname + '/datasets/' + dataset + '.json', function readFileDataClosure (err, data) {
-      if (err) {
-        return res.status(500).json({ status: 'error', message: 'Server error: Unable to read source data file' });
-      }
-
-      // Format data for specified programming language
-      // ... ?
-      var dataLang;
-      var jsonData = JSON.parse(data);
-
-      dataLang = converter.jsonToLang(jsonData, req.query.lang);
-      if (!dataLang) {
-        return res.status(400).json({ status: 'error', message: 'Unsupported lang' });
-      }
-
-      res.json({
-        status: 'ok',
-        data: dataLang
-      });
-    })
-  });
+    res.json({
+      status: 'ok',
+      data: dataLang
+    });
+  })
 });
 
 
