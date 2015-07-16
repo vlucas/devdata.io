674a93add683d0a0cfaff6c035e79860dcb3a329
mackenza <a.mackenzie@gmail.com>
2015-07-16 16:55:21 -0400

    added dart and added semicolon to js

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ var supportedFormats = {
   'python': 'Python',
   'ruby': 'Ruby',
   'csharp': 'C#',
-  'lua': 'Lua'
+  'lua': 'Lua',
+  'dart': 'Dart'
 };
 var datasets = {
   'states': 'U.S. States',
